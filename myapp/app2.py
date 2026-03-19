67a549b25f7c9032e780b8ba35d3c9f3617d517e
bomikim0214 <rlaqhals05@naver.com>
2026-03-19 16:00:19 +0900

    test: template

---
+++ myapp/app2.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('index.html', username=name)
+if __name__ == '__main__':
+    app.run(debug=True)
