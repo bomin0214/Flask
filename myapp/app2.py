246858fb177d2a8f0ff0be25198e026a5716ef88
bomikim0214 <rlaqhals05@naver.com>
2026-03-19 16:25:57 +0900

    test: 조건문, 반복문

--- myapp/app2.py
+++ myapp/app2.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('index.html', username=name)
+
+@app.route('/users')
+def users():
+    user_list = ["kim", "lee", "park"]
+    return render_template('index.html', users=user_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
